8ad00142d023d69e85e361b860cf37b5f3bd64ee
dyevri <dyevri@live.com>
2014-08-29 10:14:34 +0300

    Added array

--- index_each_do.rb
+++ index_each_do.rb
@@ -1,1 +1,7 @@
-[18:30:14]<oil>астонмартин git clone -> git branch -> git commit -> git commit -> git pull -> git merge -> git push
+#![18:30:14]<oil>астонмартин git clone -> git branch -> git commit -> git commit -> git pull -> git merge -> git push
+array_test = ["first line", "second line", "third line"]
+for item in array_test
+  puts item
+end
+
+$end
