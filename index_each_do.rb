7c5030ea464772b2d1f7f4ffc20d591ad2720d67
dyevri <dyevri@live.com>
2014-08-31 19:40:04 +0300

    Day 11 ended

--- index_each_do.rb
+++ index_each_do.rb
@@ -1,7 +1,7 @@
 #![18:30:14]<oil>астонмартин git clone -> git branch -> git commit -> git commit -> git pull -> git merge -> git push
-array_test = ["first line", "second line", "third line"]
-for item in array_test
-  puts item
-end
+favorite_candy = ['key', 'snickers', 'milky way', 'three musketeers' ]
 
-$end
+
+favorite_candy.each_with_index do |item,index|
+  puts "List item #{index+1}. #{item.capitalize}"
+  end
