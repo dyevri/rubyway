a3d9bb1422247d921821853ea2bae6081fb7da5e
dyevri <dyevri@live.com>
2014-09-01 09:36:47 +0300

    Day 12 ended

---
+++ fwnum.rb
@@ -0,0 +1,14 @@
+def factors_to_three(n)
+  remainder = n%3
+  if remainder == 0
+    puts true
+  else
+    puts false
+  end
+end
+factors_to_three(33)
+factors_to_three(99)
+factors_to_three(324723052)
+factors_to_three(1041)
+$end
+
