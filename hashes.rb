198ac03e78163aea7811653322117cfb9c474568
dyevri <dyevri@live.com>
2014-09-07 11:54:54 +0300

    Added hashes

--- hashes.rb
+++ hashes.rb
@@ -1,7 +1,9 @@
 hash = {
-    "Jackson" => 17,
-    "Sarah" => 19,
-    "Johnson" => 25,
-    "Mike" => 75
+    Jackson: 17,
+    Sarah: 19,
+    Johnson: 25,
+    Mike: 75
 }
-puts hash["Jackson"]
+old_enough = hash.select {|k, v| v > 20}
+puts old_enough
+names = hash.each_value {|k| puts k}
